b7fad22d4412ae44f690afaa5212bfc57a8f6184
dmitriybusko <dmitriy.busko@gmail.com>
2017-05-26 20:10:12 +0300

    a

--- script.js
+++ script.js
@@ -8,14 +8,3 @@ $(document).ready(function () {
     });
 
 });
-
-//Супер задание - 77 символов (76 без пробелов)
-
-function isPalindrome(s){
-    s=s.replace(/\W/g,"").toLowerCase();return s==s.split("").reverse().join("");
-}
-
-
-console.log(isPalindrome("level")); // logs 'true'
-console.log(isPalindrome("levels")); // logs 'false'
-console.log(isPalindrome("A car, a man, a maraca")); // logs 'true'
